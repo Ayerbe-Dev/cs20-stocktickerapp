05aa3da44483664249335595c9cc4dc863011bfe
Henry Nissen <63805100+Ayerbe-Dev@users.noreply.github.com>
2025-04-22 22:20:40 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./home.html', function (err, html) {
+
+    if (err) throw err;    
+
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(process.env.PORT || 3000);
+});
